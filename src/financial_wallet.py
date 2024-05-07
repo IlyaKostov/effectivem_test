b524e33f184f4266e0882827f757de5525ad877f
Kostov Ilya <kick1991@mail.ru>
2024-05-07 23:13:21 +0500

    add financial_wallet.py, data_manager.py

---
+++ src/financial_wallet.py
@@ -0,0 +1,18 @@
+class FinancialWallet:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return f''
+
+    def add_operation(self, operation):
+        pass
+
+    def edit_operation(self):
+        pass
+
+    def search_operation(self):
+        pass
+
+    def get_balance(self):
+        return f''
